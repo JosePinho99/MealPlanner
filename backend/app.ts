d4995f4ccf250248c336c59c6faf20094e4891ca
Jose Pinho <jomisapi1999@gmail.com>
2024-08-29 11:57:42 +0100

    -Connect to database

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, {query, Request, Response} from 'express';
 import cors from 'cors';
 import {GeneratedPlan, PlannedDay} from "../commons/interfaces";
 import {generatePlan} from "./core/planner";
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+    connectionString,
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 const port = 3000;
+
 
 
 app.post('/createPlan', (req: Request, res: Response) => {
@@ -16,6 +24,8 @@ app.post('/createPlan', (req: Request, res: Response) => {
 });
 
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    const result = await pool.query('select * from public."Users"');
+    console.log(result.rows);
     console.log(`App listening at http://localhost:${port}`);
 });
