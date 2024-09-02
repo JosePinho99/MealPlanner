08c3d78444b19901e12afe2f28bbfe86711f6ac4
Jose Pinho <jomisapi1999@gmail.com>
2024-09-02 18:44:34 +0100

    -Ingredients module start

--- frontend/src/app/api/ingredients.service.ts
+++ frontend/src/app/api/ingredients.service.ts
@@ -11,6 +11,13 @@ export class IngredientsService {
   constructor(private http: HttpClient) {
   }
 
+  //TODO - migrate default ingredients
+  //       develop toastr for errors
+  //       cancel buttons on create and edit
+  //       create and edit (when not logged)
+  //       create and edit (when logged)
+
+
   getIngredients() {
     return this.http.get<Ingredient[]>("http://localhost:3000/ingredients", {headers: {'token': localStorage.getItem('token') ?? 'none'}}).pipe(
       catchError(_ => of(null))
--- frontend/src/app/content/utils.ts
+++ frontend/src/app/content/utils.ts
@@ -17,28 +17,29 @@ import { improvePlan } from "../../../../commons/improve";
 export function defaultNewPlan() {
   let newPlan: NewPlan = {
     dailyRestrictions: [
-      {element: 'calories', operator: Operator.LessThan, value: ['1800']},
-      {element: 'price', operator: Operator.LessThan, value: ['12']},
-      {element: 'proteins', operator: Operator.MoreThan, value: ['75']}
+      {element: 'calories', operator: Operator.LessThan, value: ['1500']},
+      {element: 'price', operator: Operator.LessThan, value: ['11']},
+      {element: 'proteins', operator: Operator.MoreThan, value: ['50']}
     ],
     ingredientRestrictions: [
-      {element: 'Granola', operator: Operator.Combine, value: ['Leite aveia']},
-      {element: 'Massa', operator: Operator.LessThanDaily, value: ['4']},
-      {element: 'Massa', operator: Operator.DontCombine, value: ['Grão de bico']},
-      {element: 'Massa', operator: Operator.DontCombine, value: ['Feijão preto']},
-      {element: 'Compal', operator: Operator.DontCombine, value: ['Cereais']},
-      {element: 'Batatas fritas', operator: Operator.LessThanWeekly, value: ['3']},
-      {element: 'Bacon', operator: Operator.MoreThanWeekly, value: ['0']},
-      {element: 'Compal', operator: Operator.MoreThanWeekly, value: ['0']},
-      {element: 'Shots proteina', operator: Operator.LessThanWeekly, value: ['4']},
-      {element: 'All', operator: Operator.LessThanDaily, value: ['6']},
+      // {element: 'Granola', operator: Operator.Combine, value: ['Leite aveia']},
+      // {element: 'Massa', operator: Operator.LessThanDaily, value: ['4']},
+      // {element: 'Massa', operator: Operator.DontCombine, value: ['Grão de bico']},
+      // {element: 'Massa', operator: Operator.DontCombine, value: ['Feijão preto']},
+      // {element: 'Compal', operator: Operator.DontCombine, value: ['Cereais']},
+      // {element: 'Batatas fritas', operator: Operator.LessThanWeekly, value: ['3']},
+      // {element: 'Bacon', operator: Operator.MoreThanWeekly, value: ['0']},
+      // {element: 'Compal', operator: Operator.MoreThanWeekly, value: ['0']},
+      // {element: 'Shots proteina', operator: Operator.LessThanWeekly, value: ['4']},
+      {element: 'All', operator: Operator.LessThanWeekly, value: ['4']},
+      {element: 'All', operator: Operator.MoreThanWeekly, value: ['1']},
       {element: 'All', operator: Operator.DontRepeatInARow}
     ],
     meals: [
         {name: 'lunch', type: MealType.MAIN, mIMin: 1, mIMax: 1, sIMin: 1, sIMax: 1, eIMin: 1, eIMax: 1},
-        {name: 'lanche', type: MealType.SECONDARY, mIMin: 1, mIMax: 1, sIMin: 1, sIMax: 1, eIMin: 0, eIMax: 0},
+        // {name: 'lanche', type: MealType.SECONDARY, mIMin: 1, mIMax: 1, sIMin: 1, sIMax: 1, eIMin: 0, eIMax: 0},
         {name: 'dinner', type: MealType.MAIN, mIMin: 1, mIMax: 1, sIMin: 1, sIMax: 1, eIMin: 1, eIMax: 1},
-        {name: 'snack', type: MealType.SNACK, mIMin: 0, mIMax: 1, sIMin: 0, sIMax: 0, eIMin: 0, eIMax: 0}
+        // {name: 'snack', type: MealType.SNACK, mIMin: 0, mIMax: 1, sIMin: 0, sIMax: 0, eIMin: 0, eIMax: 0}
     ]
   }
   return newPlan;
